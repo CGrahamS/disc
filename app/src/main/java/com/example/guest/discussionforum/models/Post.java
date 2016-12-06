b5ab1a7c9e95caeaf294746818c601764c0f10bb
Blake Scala and Caleb Stevenson <caleb@>
2016-12-06 11:05:23 -0800

    add post model and activity for adding new posts

---
+++ app/src/main/java/com/example/guest/discussionforum/models/Post.java
@@ -0,0 +1,30 @@
+package com.example.guest.discussionforum.models;
+
+import java.security.Timestamp;
+
+/**
+ * Created by Guest on 12/6/16.
+ */
+public class Post {
+    private String postTitle;
+    private String postContent;
+    private Timestamp postTime;
+
+    public Post(String postTitle, String postContent, Timestamp postTime) {
+        this.postTitle = postTitle;
+        this.postContent = postContent;
+        this.postTime = postTime;
+    }
+
+    public String getPostTitle() {
+        return postTitle;
+    }
+
+    public String getPostContent() {
+        return postContent;
+    }
+
+    public Timestamp getPostTime() {
+        return postTime;
+    }
+}
--- app/src/main/java/com/example/guest/discussionforum/ui/CategoryDetailActivity.java
+++ app/src/main/java/com/example/guest/discussionforum/ui/CategoryDetailActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -15,10 +17,11 @@ import org.parceler.Parcels;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class CategoryDetailActivity extends AppCompatActivity {
+public class CategoryDetailActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.categoryDetailNameTextView) TextView mCategoryName;
     @Bind(R.id.categoryDetailDescriptionTextView) TextView mCategoryDescription;
     @Bind(R.id.categoryDetailPostListView) ListView mPostList;
+    @Bind(R.id.newPostButton) Button mNewPostButton;
     Category mCategory;
 
     @Override
@@ -33,5 +36,12 @@ public class CategoryDetailActivity extends AppCompatActivity {
         mCategoryName.setText(mCategory.getName());
         mCategoryDescription.setText(mCategory.getDescription());
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mNewPostButton)  {
+            Intent intent = new Intent(CategoryDetailActivity.this, NewPostActivity.class);
+        }
     }
 }
